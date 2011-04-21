3140d4986b6dd50488f96cc892ab92789cbb0476
unknown <curran@.(none)>
2011-04-20 17:05:39 -0700

    modified index page

--- ProcessingDB/grails-app/conf/BootStrap.groovy
+++ ProcessingDB/grails-app/conf/BootStrap.groovy
@@ -2,11 +2,9 @@ import grails.converters.*
 import org.curransoft.processingdb.Script;
 class BootStrap {
     def init = { servletContext ->
-
         String filePath = "WEB-INF/data/dbArchive11-04-17-17-02.xml";
         def data = servletContext.getResourceAsStream(filePath);
         XML.parse(data,"UTF-8").script.each{
-            println("name = "+it.name[0]);
             org.curransoft.processingdb.Script s = new org.curransoft.processingdb.Script(
                 name:it.name[0].toString(),
                 description:it.description.toString(),
@@ -14,7 +12,6 @@ class BootStrap {
                 doc:it.doc.toString(),
                 isApp:(it.isApp == true)
             )
-            
             it.dependencies.script.@id.each{
                 int id = Integer.parseInt(it.toString())
                 s.addToDependencies(Script.get(id));
--- ProcessingDB/grails-app/views/index.gsp
+++ ProcessingDB/grails-app/views/index.gsp
@@ -1,12 +1,15 @@
 <html>
     <head>
-        <title>Welcome to Grails</title>
+        <title>ProcessingDB</title>
         <meta name="layout" content="main" />
     </head>
     <body>
         <div>
             <h1>Welcome to ProcessingDB!</h1>
-
+            A pre-alpha stage project aimed to become a global wiki for <a href="http://processing.org/">Processing</a> code, enabling mass-collaborative development of web-embeddable graphics and information visualization programs.
+            <br><br>
+            <a href="http://curransoft.com/code/2011/04/processingdb/"> Click here </a> to learn more about this site.
+<br><br>
             <g:link controller="script">Scripts</g:link>
             <ul>
             <g:each var="s" in="${org.curransoft.processingdb.Script.list()}">
