4f3548b115bcb3202fe0c03165337783269a4dc5
Curran Kelleher <curran.kelleher@gmail.com>
2012-01-31 20:53:51 -0500

    Added JQuery and canvas test to hello CoffeeScript

--- experiments/HelloCoffeeScript/coffee/test.coffee
+++ experiments/HelloCoffeeScript/coffee/test.coffee
@@ -1,1 +1,11 @@
-console.log 'Hello World!'
+resize = () ->
+  canvas = $('#c')[0]
+  canvas.width  = window.innerWidth
+  canvas.height = window.innerHeight
+  c = canvas.getContext('2d');
+  c.moveTo 0, 0
+  c.lineTo 100, 100
+  c.stroke()
+
+$(document).ready resize
+$(window).resize resize
--- experiments/HelloCoffeeScript/public/test.js
+++ experiments/HelloCoffeeScript/public/test.js
@@ -1,5 +1,19 @@
 (function() {
+  var resize;
 
-  console.log('Hello World!');
+  resize = function() {
+    var c, canvas;
+    canvas = $('#c')[0];
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    c = canvas.getContext('2d');
+    c.moveTo(0, 0);
+    c.lineTo(100, 100);
+    return c.stroke();
+  };
+
+  $(document).ready(resize);
+
+  $(window).resize(resize);
 
 }).call(this);
