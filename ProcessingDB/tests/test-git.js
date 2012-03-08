76b1352911e44da17769a69786a2f4de0b4fe1d6
curran <curran.kelleher@gmail.com>
2012-03-08 15:10:56 -0500

    began implementing Git module, wrote first unit tests

---
+++ ProcessingDB/tests/test-git.js
@@ -0,0 +1,19 @@
+var git = require('../modules/git');
+var async = require('async');
+
+exports.testRepoDirFns = function(test) {
+  async.waterfall([git.repoDirExists,
+  function(exists, callback) {
+    test.ok(!exists, "Repo dir should not exist initially.");
+    git.createRepoDir(callback);
+  }, git.repoDirExists,
+  function(exists, callback) {
+    test.ok(exists, "Repo dir should exist now.");
+    git.deleteRepoDir(callback);
+  }, git.repoDirExists,
+  function(exists, callback) {
+    test.ok(!exists, "Repo dir should no longer exist.");
+    test.done();
+  }]);
+
+};
