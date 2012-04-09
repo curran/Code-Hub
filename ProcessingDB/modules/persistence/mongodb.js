b4dc5b755cadf5cc7000d13cebcf8e18c3deedc5
curran <curran.kelleher@gmail.com>
2012-04-09 18:25:41 -0400

    began implementing storage of Revision instances

--- ProcessingDB/modules/persistence/mongodb.js
+++ ProcessingDB/modules/persistence/mongodb.js
@@ -21,39 +21,30 @@ var RevisionPointers = new Schema({
 });
 
 var Revisions = new Schema({
-  scriptId: Number,
-  revNum: Number,
+  _id: String, // scriptId.revNum
   commitMessage: String,
   commitDate: Date,
-  
-  // for tracking branching
-  parentRevision: {
-    scriptId: Number,
-    revNum: Number
-  },
-  
+  parentRevision: String, // "scriptId.revNum" or ""
   type: String, // in ['module','app','template']
   
   // relevant when (type == 'module' || type == 'template')
   name: String,
   
-  // relevant when type == 'module'
-  // contains direct dependencies by module name
-  moduleDependencies: [String],
+  // relevant when type == 'module' or type == 'app'
+  // if 'module', contains direct dependencies by module name
+  //   of the form "moduleA,moduleB,moduleC"
+  // if 'app', contains transitive dependencies by revision id
+  //   of the form "scriptIdA.revNumA,scriptIdB.revNumB"
+  //   e.g. "4.2,6.3,8.1"
+  // or "" for no dependencies or if type == 'template'
+  dependencies: String,
   
   // relevant when type == 'app'
-  // contains all transitive dependencies
-  appDependencies: [RevisionPointers],
-  
-  // relevant when type == 'app'
-  template: {
-    scriptId: Number,
-    revNum: Number
-  }
+  template: String // "scriptId.revNum" or ""
 });
 
 Counters.statics.findAndModify = function (query, sort, doc, options, callback) {
-    return this.collection.findAndModify(query, sort, doc, options, callback);
+  return this.collection.findAndModify(query, sort, doc, options, callback);
 };
 
 mongoose.connect('mongodb://localhost/'+dbName,function(err){
@@ -81,25 +72,65 @@ function incrementCounter(schemaName, callback){
   });
 }
 
-// creates a new script
-// callback(err, scriptId)
+/**
+ * Creates a new script.
+ * callback(err, scriptId)
+ */
 exports.createScript = function(callback){
   incrementCounter('Script',callback);
 };
 
-// Clears all database content
+/**
+ * Clears all database content.
+ * callback(err)
+ */
 exports.clearDB = function(callback){
   Script.remove({},function(){
     Counter.remove({},callback);
   });
 }
 
-// Disconnects from MongoDB so the Node process can end.
+/**
+ * Disconnects from MongoDB so the Node process can end.
+ * For unit testing.
+ */
 exports.disconnect = function(){
   mongoose.disconnect();
 }
 
+/**
+ * Sets the name of the MongoDB database to be used.
+ * For unit testing.
+ */
 exports.setDbName = function(newDbName){
   dbName = newDbName;
 }
-
+/**
+ * Creates a new revision.
+ * @param {scriptId} The id of the script for which to 
+ *   create the new revision.
+ * @param {revisionObject} The content to be persisted,
+ *   expected to contain the following fields:
+ *  - scriptId
+ *  - revNum
+ *  - commitMessage: String
+ *  - commitDate: Date,
+ *  - parentRevision: String, "scriptId.revNum" or ""
+ *  - type: String, // in ['module','app','template']
+ *  - name: String,
+ *    relevant when (type == 'module' || type == 'template')
+ *  - dependencies: String,
+ *    relevant when type == 'module' or type == 'app'
+ *    if 'module', contains direct dependencies by module name
+ *      of the form "moduleA,moduleB,moduleC"
+ *    if 'app', contains transitive dependencies by revision id
+ *      of the form "scriptIdA.revNumA,scriptIdB.revNumB"
+ *      e.g. "4.2,6.3,8.1"
+ *    or "" for no dependencies or if type == 'template'
+ *  - template: String // "scriptId.revNum" or ""
+ *    relevant when type == 'app'
+ * @param callback(err, revNum) Passes the new revision number
+ */
+exports.createRevision = function(scriptId, revisionObject, callback){
+  
+}
--- ProcessingDB/tests/test-mongodb.js
+++ ProcessingDB/tests/test-mongodb.js
@@ -1,23 +1,55 @@
 var db = require('../modules/persistence/mongodb');
-var testDbName = 'ProcessingDB-Test';
+var async = require('async');
 
+db.setDbName('ProcessingDB-Test');
+
+/**
+ * Tests incrementing script ids.
+ */
 exports.testCreateScript = function(test) {
-  db.setDbName(testDbName);
-  db.createScript(function(err, scriptId){
-    if(err){
-      console.log(err);
-      test.fail();
-    }
-    test.equal(scriptId, 1 , "First Script id should be 1");
-    db.createScript(function(err, scriptId){
+  async.waterfall([
+    db.createScript,
+    function(scriptId, callback){
+      test.equal(scriptId, 1 , "First Script id should be 1");
+      db.createScript(callback);
+    },
+    function(scriptId, callback){
       test.equal(scriptId, 2 , "Second Script id should be 2");
-      db.createScript(function(err, scriptId){
-        test.equal(scriptId, 3 , "Third Script id should be 3");
-        db.clearDB(function(err){
-          db.disconnect();
-          test.done();
-        });
-      });
-    });
-  });
+      db.createScript(callback);
+    },
+    function(scriptId, callback){
+      test.equal(scriptId, 3 , "Third Script id should be 3");
+      db.clearDB(callback);
+    },
+    function(callback){
+      test.done();
+    }
+  ]);
+};
+
+exports.testRevisionWriteRead = function(test) {
+  async.waterfall([
+    db.createScript,
+    function(scriptId, callback){
+      test.equal(scriptId, 1 , "First Script id should be 1");
+      // db.createRevision({
+        // scriptId:scriptId,
+        // commitMessage: "Test Commit Message",
+        // commitDate: new Date(2012, 4, 9),
+        // parentRevision: "",
+        // type: 'template',
+        // name: 'TestTemplate',
+        // dependencies: "",
+        // template: ""
+      // }, callback);
+    // },
+    // function(revNum, callback){
+      // test.equal(revNum, 1 , "First revNum should be 1");
+      db.clearDB(callback);
+    },
+    function(callback){
+      db.disconnect();
+      test.done();
+    }
+  ]);
 };
