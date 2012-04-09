fefd0e1b84dbaeca0f2ae6b23d07a88d95b6373b
curran <curran.kelleher@gmail.com>
2012-04-09 17:17:43 -0400

    Got autoincrement working with a single DB call

--- ProcessingDB/modules/persistence/mongodb.js
+++ ProcessingDB/modules/persistence/mongodb.js
@@ -5,21 +5,63 @@ var mongoose = require('mongoose'),
 // May be changed for unit testing.
 var dbName = 'processingDB';
 
-var Scripts = new Schema({
-  scriptId: Number,
-  latestRevNum: Number
-});
-
 var Counters = new Schema({
   _id:String, // the schema name
   count: Number
 });
 
-mongoose.connect('mongodb://localhost/'+dbName,function(err){
-  if(err)
-    throw "Failed to connect to MongoDB. Is mongod running?";
+var Scripts = new Schema({
+  _id: Number, //scriptId
+  latestRevNum: Number
 });
+
+var RevisionPointers = new Schema({
+  scriptId: Number,
+  revNum: Number
+});
+
+var Revisions = new Schema({
+  scriptId: Number,
+  revNum: Number,
+  commitMessage: String,
+  commitDate: Date,
+  
+  // for tracking branching
+  parentRevision: {
+    scriptId: Number,
+    revNum: Number
+  },
+  
+  type: String, // in ['module','app','template']
+  
+  // relevant when (type == 'module' || type == 'template')
+  name: String,
+  
+  // relevant when type == 'module'
+  // contains direct dependencies by module name
+  moduleDependencies: [String],
+  
+  // relevant when type == 'app'
+  // contains all transitive dependencies
+  appDependencies: [RevisionPointers],
+  
+  // relevant when type == 'app'
+  template: {
+    scriptId: Number,
+    revNum: Number
+  }
+});
+
+Counters.statics.findAndModify = function (query, sort, doc, options, callback) {
+    return this.collection.findAndModify(query, sort, doc, options, callback);
+};
+
+mongoose.connect('mongodb://localhost/'+dbName,function(err){
+  if(err) throw "Failed to connect to MongoDB. Is mongod running?";
+});
+
 var Counter = mongoose.model('Counter', Counters);
+
 var Script = mongoose.model('Script', Scripts);
 
 /**
@@ -30,18 +72,12 @@ var Script = mongoose.model('Script', Scripts);
  *   count (a Number).
  */
 function incrementCounter(schemaName, callback){
-  // TODO fix this so it is just one atomic db call
-  Counter.update({ _id: schemaName }, { $inc: {count: 1 } },
-                 { upsert: true }, function(err, counter){
-    if(err)
-      callback(err);
-    else
-      Counter.findOne({ _id: schemaName }, function(err, counter){
-        if(err)
-          callback(err);
-        else
-          callback(null, counter.count.valueOf());
-      });
+  Counter.findAndModify({ _id: schemaName }, [], 
+    { $inc: { count: 1 } }, {"new":true, upsert:true}, function (err, result) {
+      if (err)
+        callback(err);
+      else
+        callback(null, result.count);
   });
 }
 
@@ -63,32 +99,7 @@ exports.disconnect = function(){
   mongoose.disconnect();
 }
 
-// var RevisionPointers = new Schema({
-  // scriptId: Number,
-  // revNum: Number
-// });
-// 
-// var Revisions = new Schema({
-  // scriptId: Number,
-  // revNum: Number,
-  // commitMessage: String,
-  // commitDate: Date,
-  // // from whence this came, for tracking branching
-  // parentRevision: {
-    // scriptId: Number,
-    // revNum: Number
-  // },
-  // type: String, // in ['module','app','template']
-//   
-  // // relevant when (type == 'module' || type == 'template')
-  // name: String,
-//   
-  // // relevant when (type == 'app' || type == 'module')
-  // dependencies: [RevisionPointers],
-//   
-  // // relevant when type == 'app'
-  // template: {
-    // scriptId: Number,
-    // revNum: Number
-  // }
-// });
+exports.setDbName = function(newDbName){
+  dbName = newDbName;
+}
+
--- ProcessingDB/tests/test-mongodb.js
+++ ProcessingDB/tests/test-mongodb.js
@@ -1,13 +1,23 @@
 var db = require('../modules/persistence/mongodb');
+var testDbName = 'ProcessingDB-Test';
 
 exports.testCreateScript = function(test) {
+  db.setDbName(testDbName);
   db.createScript(function(err, scriptId){
-    console.log('here');
+    if(err){
+      console.log(err);
+      test.fail();
+    }
     test.equal(scriptId, 1 , "First Script id should be 1");
-    //test.done();
-    db.clearDB(function(err){
-      db.disconnect();
-      test.done();
+    db.createScript(function(err, scriptId){
+      test.equal(scriptId, 2 , "Second Script id should be 2");
+      db.createScript(function(err, scriptId){
+        test.equal(scriptId, 3 , "Third Script id should be 3");
+        db.clearDB(function(err){
+          db.disconnect();
+          test.done();
+        });
+      });
     });
   });
 };
