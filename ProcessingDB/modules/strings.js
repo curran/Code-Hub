29e1499102dd3d4c87de0d6bcae54bcf9ad5deb8
curran <curran.kelleher@gmail.com>
2012-04-15 19:17:34 -0400

    added error page and strings module, handled error when looking up nonexistent revision

---
+++ ProcessingDB/modules/strings.js
@@ -0,0 +1,7 @@
+/**
+ * This module contains all the strings which are
+ * included in user-facing content such as error messages.
+ */
+exports.revNotFound = function(scriptId, revNum){
+  return "No revision exists with script id "+scriptId+" and revision number "+revNum+".";
+};
